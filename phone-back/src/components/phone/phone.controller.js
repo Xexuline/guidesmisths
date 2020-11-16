0dd0ed4720ea47561a4560c24942688744ffcaa8
Jesús Sabroso <jesussabroso@PX1003-JSABROSO.local>
2020-11-16 20:55:29 +0100

    remove validation before save

--- phone-back/src/components/phone/phone.controller.js
+++ phone-back/src/components/phone/phone.controller.js
@@ -1,18 +1,13 @@
 const phone = require('./phone.model');
 
-exports.savePhone = (phoneInfo) => {
-  if (phoneInfo == null || Object.keys(phoneInfo).length === 0 || phoneInfo.length <= 0) {
-    return Promise.reject(new Error('empty-phone-info'));
-  }
-
-  return phone.add(phoneInfo);
-};
+exports.savePhone = (phoneInfo) => phone.add(phoneInfo);
 
 exports.getPhones = (id) => {
   if (id) {
-    console.log('id:', id);
     return phone.getById(id);
   }
 
   return phone.getList();
 };
+
+exports.updatePhone = (phoneId, phoneInfo) => phone.update(phoneId, phoneInfo);
