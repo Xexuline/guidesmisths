1853f184f4637312c6b53366d5f7a715d81954df
Jesús Sabroso <jesussabroso@PX1003-JSABROSO.local>
2020-11-18 22:58:02 +0100

    allow upload images

--- phone-back/server.js
+++ phone-back/server.js
@@ -1,19 +1,22 @@
 const bodyParser = require('body-parser');
-const app = require('express')();
+const express = require('express')
 const helmet = require('helmet');
 const cors = require('cors')
 const config = require('./config');
 const routes = require('./src/router');
 const mongo = require('./src/components/mongo');
 
+const app = express();
 const port = process.env.PORT || 80;
 const host = `${config.defaultServer.host || ' localhost'}:${config.defaultServer.port}`;
 
 app.use(helmet());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use('/api/v1', cors(), routes);
+app.use('/', express.static(`${__dirname}/public`));
+
 
 app.listen(port, () => { console.log(`running on ${host}`); });
 
