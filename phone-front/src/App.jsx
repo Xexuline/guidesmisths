a1d3ff12d19ab19b119452164f354ea4de51ae92
Jesús Sabroso <jesussabroso@PX1003-JSABROSO.local>
2020-11-18 13:35:31 +0100

    change service call to it component

--- phone-front/src/App.jsx
+++ phone-front/src/App.jsx
@@ -2,13 +2,10 @@
 import React, { Component } from 'react'
 import './App.scss';
 import Header from './components/header/header'
-import Footer from './components/footer/footer'
-import Layout from './components/layout/layout'
 import PhonesContainer from './containers/phonesContainer/phonesContainer'
 import DescriptionContainer from './containers/descriptonContainer/descriptionContainer'
 import Modal from './components/modal/modal'
 import Spinner from './components/loader/loader'
-import { PhoneService } from './services/phone'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 class App extends Component {
@@ -16,56 +13,46 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      loading: false,
-      phoneList: []
+      loading: false
     }
 
-    this.getPhonesList = this.getPhonesList.bind(this)
+    this.loader = this.loader.bind(this)
   }
 
-  getPhonesList() {
-    this.setState({ loading: true })
-    PhoneService.getList()
-      .then(({data: phoneList}) => {
-        this.setState({ 
-          loading: false,
-          phoneList
-        })
+
+  loader(isLoading) {
+    if(this.state.loading !== isLoading){
+      this.setState({
+        loading: isLoading
       })
-  }
-
-  componentDidMount() {
-    this.getPhonesList()
+    }
   }
 
   lockBody(loading){
     const body = document.querySelector('body')
-    // debugger
     body.classList[loading ? 'add' : 'remove']('modal-open')
   }
 
   componentDidUpdate(prevProp, prevState) {
-    // debugger
     if(prevState.loading !== this.state.loading) {
       this.lockBody(this.state.loading)
     }
   }
 
   render() {
-    // const classNames = ['App', this.state.loading ? 'modal-open' : null].join(' ')
     return (
       <BrowserRouter>
         <div className='App'>
-          <div className="App__wrapper">
+          <main className="App__wrapper">
             <Header />
             <Switch>
-              <Route path="/" exact={ true } component={ () => <PhonesContainer phones={ this.state.phoneList } /> }></Route>
+              <Route path="/" exact={ true } component={ () => <PhonesContainer /> }></Route>
               <Route path="/new" exact={ true } component={ () => <div>create new</div>}></Route>
               <Route path="/update/:id" exact={ true } component={ () => <div>create new</div>}></Route>
-              <Route path="/:id" component={ () => <div>con id</div> }></Route>
+              <Route path="/:id" component={ () => <DescriptionContainer /> }></Route>
             </Switch>
             { this.state.loading ? <Modal><Spinner /></Modal> : null }
-          </div>
+          </main>
         </div>
       </BrowserRouter>
     );
--- phone-front/src/containers/phonesContainer/phonesContainer.jsx
+++ phone-front/src/containers/phonesContainer/phonesContainer.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
 import PhoneCard from '../../components/phone-card/phoneCard'
+import BackToTop from '../../components/back-to-top/back-to-top'
 import './phonesContainer.scss'
-import propTypes from 'prop-types'
+import { PhoneService } from '../../services/phone'
 
 // TODO Mock api
 class PhonesContainer extends Component {
@@ -9,21 +10,37 @@ class PhonesContainer extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      showBackToTop: false
+      showBackToTop: false,
+      phoneList: []
     }
 
     this.checkScroll = this.checkScroll.bind(this)
+    this.getPhonesList = this.getPhonesList.bind(this)
   }
 
-  checkScroll(e) {
-    // const firstElement = document.querySelector('.phone-card__wrapper')
-    
-    // console.log(firstElement)
-    // console.log('offsetTop', firstElement.offsetTop, 'scrollTop', firstElement.scrollTop, 'clientTop', firstElement.clientTop)
-    // console.log(e)
+  checkScroll() {
+    const scrollTop = document.body.scrollTop || document.documentElement.scrollTop
+    const headerHeight = document.querySelector('.header').clientHeight
+
+    if (scrollTop >= headerHeight) {
+      !this.state.showBackToTop && this.setState({ showBackToTop: true }, () => { console.log('cambiado,', true)})
+    } else {
+      this.state.showBackToTop && this.setState({ showBackToTop: false }, () => { console.log('cambiado,', false)})
+    }
+  }
+
+  getPhonesList() {
+    PhoneService
+      .getList()
+      .then(({data: phoneList}) => {
+        this.setState({ 
+          phoneList
+        })
+      })
   }
 
   componentDidMount() {
+    this.getPhonesList()
     window.addEventListener('scroll', this.checkScroll)
   }
 
@@ -32,20 +49,23 @@ class PhonesContainer extends Component {
   }
 
   render() {
-    const { phones } = this.props
+    const { phoneList } = this.state
+
     return (
-      <div className="phone-container">
-        { phones.map(el => 
-          <PhoneCard key={`phone-${el._id}`} { ...el }/>
-        ) }
-      </div>
+      <article className="phone-container__wrapper">
+        <h2> Phone List</h2>
+        <div className="phone-container">
+          { phoneList.map(el => 
+            <PhoneCard key={`phone-${el._id}`} { ...el }/>
+          ) }
+        </div>
+        <BackToTop visible={ this.state.showBackToTop }/>
+      </article>
 
     )
   }
 }
 
-PhonesContainer.propTypes = {
-  phones: propTypes.array
-}
+PhonesContainer.propTypes = {}
 
 export default PhonesContainer
