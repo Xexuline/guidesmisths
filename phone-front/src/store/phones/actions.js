9eb5dd4484767279499d8796a3acf52a603d91e6
Jesús Sabroso <jesussabroso@PX1003-JSABROSO.local>
2020-11-21 19:38:42 +0100

    change call action to async /await

--- phone-front/src/store/phones/actions.js
+++ phone-front/src/store/phones/actions.js
@@ -1,13 +1,11 @@
 import { PhoneService } from '../../services/phone'
 
 export const SET_PHONE_LIST= 'SET_PHONE_LIST'
+export const SET_PHONE_DESC= 'SET_PHONE_DESC'
 
 export class ThunkActions {
-  static getPhones = () => (dispatch) => {
-    return PhoneService
-    .getList()
-    .then(({data: phoneList}) => {
-      dispatch({type: SET_PHONE_LIST, payload: phoneList })
-    })
+  static getPhones = () => async (dispatch) => {
+    const { data: phoneList } = await PhoneService.getList()
+    dispatch({type: SET_PHONE_LIST, payload: phoneList })
   }
 }
