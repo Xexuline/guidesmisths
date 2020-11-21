3529a2976cc76433012b074517aaa785fc979bbf
Jesús Sabroso <jesussabroso@PX1003-JSABROSO.local>
2020-11-21 12:13:01 +0100

    render phone description

--- phone-front/src/containers/descriptonContainer/descriptionContainer.jsx
+++ phone-front/src/containers/descriptonContainer/descriptionContainer.jsx
@@ -4,6 +4,8 @@ import { connect } from 'react-redux'
 import './descriptionContainer.scss'
 import { PhoneService } from '../../services/phone'
 import { setLoading } from '../../store/ui/actions'
+import noPhponeimg from '../../assets/img/svg/noPhone.svg'
+import emptyImage from '../../assets/img/svg/empty.svg'
 
 class DescriptionContainer extends Component {
 
@@ -16,7 +18,7 @@ class DescriptionContainer extends Component {
         description: '',
         color: '',
         price: '',
-        imageFileName: '',
+        imageFileName: emptyImage,
         screen: '',
         processor: '',
         ram: ''
@@ -24,23 +26,72 @@ class DescriptionContainer extends Component {
     }
   }
 
+  phoneAttributeGenerator(description, content) {
+    if (!content || ['imageFileName', 'description'].includes(description) ) {
+      return null
+    }
+
+    return (
+    <div key={ description } className="description__attributes--box">
+      <span className="description__attributes--description">{ description }</span>
+      <span className="description__attributes--content">{ content }</span>
+    </div>)
+
+  }
+
   componentDidMount(){
     if(this.props.match) {
       this.props.setLoading(true)
       const { id } = this.props.match.params
+
       PhoneService.getInfo(id).then(({data: info}) => {
-        this.setState({ phoneInfo: info })
+        const { description,
+          imageFileName,
+          color,
+          price,
+          screen,
+          processor,
+          ram } = info
+        this.setState({ phoneInfo: {
+          description,
+          imageFileName: imageFileName || '',
+          color,
+          price,
+          screen,
+          processor,
+          ram }})
         this.props.setLoading(false)
       })
     }
   }
 
   render() {
+    const { phoneInfo } = this.state
     return (
-      <>
-        <div>description container</div>
-    <div>{ Object.keys(this.state.phoneInfo).map(el => <div key={el}>{ el } - { this.state.phoneInfo[el] }</div>)}</div>
-      </>
+      <article className="description__wrapper">
+        <div className="description__top">
+          <img className="description__image" src={  phoneInfo.imageFileName ? `http://localhost:3001/uploads/${phoneInfo.imageFileName}` : noPhponeimg } alt=""/>
+          <div className="description__header">
+            <h2>{ phoneInfo.name }</h2>
+            <h3>{ phoneInfo.manufacturer }</h3>
+          </div>
+        </div>
+        <div className="description__bottom">
+          <div className="description__header">
+            <h2>{ phoneInfo.name }</h2>
+            <h3>{ phoneInfo.manufacturer }</h3>
+          </div>
+          <div className="description__attributes">
+            { Object.keys(this.state.phoneInfo).map(phoneAttribute => this.phoneAttributeGenerator(phoneAttribute, phoneInfo[phoneAttribute])) }
+          </div>
+          { 
+            !phoneInfo.description ? null :
+            <div>
+              <p><span className="description__paragraph">Description:</span> <span>{ phoneInfo.description }</span></p>
+            </div>
+          }
+        </div>
+      </article>
     )
   }
 }
